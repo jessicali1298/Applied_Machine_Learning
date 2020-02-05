2601222e131e4ba44adbd6bdd5824f9b24ec3745
clairel0130 <Lxj302552560>
2020-02-05 12:51:31 -0500

    log reg done (for now)

--- Project1/main_test.py
+++ Project1/main_test.py
@@ -3,10 +3,10 @@ import pandas as pd
 import numpy as np
 import Data_Cleaner as cd
 import Log_Regression as lgr
+from sklearn.linear_model import LogisticRegression
 
 
-
-root_path = '/Users/j.li/School/U4_WINTER/COMP 551/Applied_Machine_Learning/Project1/'
+root_path = '/Users/liuxijun/Downloads/Applied_Machine_Learning/Project1/'
 path1 = root_path + 'dataset1/ionosphere.data'
 path2 = root_path + 'dataset2/adult.data'
 path3 = root_path + 'dataset3/breast-cancer-wisconsin.data'
@@ -59,7 +59,7 @@ dataset4_clean.update(new_df)
 
 # - drop 1st, 2nd, and last features, which are vendor name, model name, and estimated relative performance
 dataset4_clean = dataset4_clean.drop(columns = {0,1,9})
-print(dataset4_clean.iloc[0:20,:])
+#print(dataset4_clean.iloc[0:20,:])
 
 
 
@@ -67,10 +67,13 @@ print(dataset4_clean.iloc[0:20,:])
 
 
 
-
+#print(dataset1_clean.columns)
 # extract labels (y)
-y1 = dc.extract_y(dataset1_clean).to_numpy()
+#y1 = dc.extract_y(dataset1_clean).to_numpy()
+#dataset1_clean = dataset1_clean.drop(dataset1_clean.columns[-1], axis=1)
 
+y2 = dc.extract_y(dataset2_clean).to_numpy()
+dataset2_clean = dataset2_clean.drop(dataset2_clean.columns[-1], axis=1)
 
 
 
@@ -81,6 +84,7 @@ y1 = dc.extract_y(dataset1_clean).to_numpy()
 
 
 # change all output datasets to numpy arrays
+#print(y1.shape)
 dataset1_arr = dataset1_clean.to_numpy()
 dataset2_arr = dataset1_clean.to_numpy()
 dataset2_arr_test = dataset2_clean_test.to_numpy()
@@ -89,18 +93,24 @@ dataset4_arr = dataset1_clean.to_numpy()
 
 
 
-print(y1.shape)
-print(dataset1_arr.shape)
+#print(y1)
+#print(dataset1_arr.shape)
 
 #%%
 #--------------------TEST LOGISTIC REGRESSION-----------------
 # Add 1 to X in the front
-shape0, shape1 = dataset1_arr.shape
-train_data = dataset1_arr[np.arange(int(shape0/5*4))]
-test_data = dataset1_arr[np.arange(int(shape0/5*4),shape0)]
+#shape0, shape1 = dataset1_arr.shape
+#train_data = dataset1_arr[np.arange(int(shape0/5*4))]
+#test_data = dataset1_arr[np.arange(int(shape0/5*4),shape0)]
+#train_y = y1[np.arange(int(shape0/5*4))]
+#test_y = y1[np.arange(int(shape0/5*4), shape0)]
 
-train_y = y1[np.arange(int(shape0/5*4))]
-test_y = y1[np.arange(int(shape0/5*4)),shape0]
+shape0, shape1 = dataset2_arr.shape
+train_data = dataset2_arr[np.arange(int(shape0/5*4))]
+test_data = dataset2_arr[np.arange(int(shape0/5*4),shape0)]
+train_y = y2[np.arange(int(shape0/5*4))]
+test_y = y2[np.arange(int(shape0/5*4), shape0)]
+
 
 N,m = train_data.shape
 
@@ -109,6 +119,17 @@ lg.fit(train_data, train_y, 0.1, 0.01)
 
 y_pred = lg.predict(test_data)
 
+clf = LogisticRegression()
+clf.fit(train_data, train_y)
+y_pred_ski = clf.predict(test_data)
+
 comparison = np.equal(test_y, y_pred)
-accuracy = (np.where(comparison == False)[0].size)/comparison.size
+accuracy = (np.where(comparison == True)[0].size)/comparison.size
+
+#comparison_ski = np.equal(test_y, y_pred_ski)
+#accuracy_ski = (np.where(comparison_ski == True)[0].size)/comparison_ski.size
+
+accuracy_ski = clf.score(test_data, test_y)
+
 print(accuracy)
+print(accuracy_ski)
