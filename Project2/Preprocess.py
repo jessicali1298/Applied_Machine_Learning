a37a57e1af9dce9fe5c3b523480be4c822e86b73
Jessica Li <yihui.li@mail.mcgill.ca>
2020-03-02 17:05:58 -0500

    added TFIDF Vectorizer

--- Project2/Preprocess.py
+++ Project2/Preprocess.py
@@ -5,7 +5,24 @@ import pandas as pd
 import re
 import string
 
-root_path = '/Users/j.li/School/U4_WINTER/COMP 551/Applied_Machine_Learning/Project2/'
+# import and download NLP Resources
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
+import nltk
+nltk.download('stopwords')
+nltk.download('punkt')
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
+
+#root_path = '/Users/j.li/School/U4_WINTER/COMP 551/Applied_Machine_Learning/Project2/'
+
+#%% 
+# Define a dummy tokenizer for TF-IDF Vectorizer
+def dummy_tok(input):
+    return input
 
 #%%
 from sklearn.datasets import fetch_20newsgroups
@@ -20,7 +37,7 @@ from sklearn.datasets import fetch_20newsgroups
 
 
 # only test with a few groups
-categories = ['alt.atheism', 'soc.religion.christian','comp.graphics', 'sci.med']
+categories = ['comp.graphics', 'sci.med']
 twenty_train = fetch_20newsgroups(subset='train', categories=categories, 
                                   shuffle=True, random_state=42)
 train_data = twenty_train.data
@@ -39,28 +56,41 @@ labels = twenty_train.target
 # 6. removing stop words, sparse terms, and particular words
 # 7. text canonicalization (use only root words)
 
-clean_train_data = []
-
+result = []
 for data in train_data:
-    # 1. convert all letters to lower
+    # 1. convert all letters to lower cases
     temp = data.lower()
     
-    # 2. convert numbers into words or removing numbers
-    temp = re.sub(r’\d+’, ‘’, temp)
+    # 2. remove numbers
+    temp = re.sub(r'\d+', '', temp)
 
-    # 3. removing punctuations, accent marks and other diacritics
-    temp = temp.translate(string.maketrans(“”,””), string.punctuation)
+    # 3. remove punctuations, accent marks and other diacritics
+    temp = temp.translate(str.maketrans('','',string.punctuation))
 
-    # 4. removing white spaces
+    # 4. remove leading and ending white spaces
     temp = temp.strip()
     
-    # 5. expanding abbreviations
+    # 5. remove stop words
+    stops = set(stopwords.words("english"))
+    tokens = word_tokenize(temp)
+    filtered_words = [word for word in tokens if word not in stops]
     
+    # 6. text canonicalization (only use root words)
+    stemmer= PorterStemmer()
+    result_word = []
 
+    for word in filtered_words:
+        root_word = stemmer.stem(word)
+        result_word.append(root_word)
 
-
-
-
+    result.append(result_word)
+    
+    
+vectorizer = TfidfVectorizer(analyzer='word', tokenizer = dummy_tok, 
+                             preprocessor = dummy_tok,
+                             token_pattern = None) 
+X = vectorizer.fit_transform(result)
+features = vectorizer.get_feature_names()
 
 
 
