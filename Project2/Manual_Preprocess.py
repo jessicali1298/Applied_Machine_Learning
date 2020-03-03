bbc35d535fe4b7d6d58ce8324a15cf0d1b04cebf
Jessica Li <yihui.li@mail.mcgill.ca>
2020-03-03 15:27:29 -0500

    separated pipeline and manual classification

---
+++ Project2/Manual_Preprocess.py
@@ -0,0 +1,138 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+import pandas as pd
+import re
+import string
+
+# import and download NLP Resources
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
+import nltk
+nltk.download('stopwords')
+nltk.download('punkt')
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import cross_validate
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+
+
+#root_path = '/Users/j.li/School/U4_WINTER/COMP 551/Applied_Machine_Learning/Project2/'
+
+#%% 
+# Define a dummy tokenizer for TF-IDF Vectorizer
+def dummy_tok(input):
+    return input
+
+#%%
+from sklearn.datasets import fetch_20newsgroups
+
+#-----------------------only test with a few groups------------------------
+categories = ['comp.graphics', 'sci.med']
+twenty_train = fetch_20newsgroups(subset='train', categories=categories, 
+                                  shuffle=True, random_state=42)
+twenty_test = fetch_20newsgroups(subset='test', categories=categories, 
+                                 shuffle=True, random_state=42)
+twenty_all = fetch_20newsgroups(subset='all', categories=categories, 
+                                 shuffle=True, random_state=42)
+train_data = twenty_train.data
+train_labels = twenty_train.target
+
+test_data = twenty_test.data
+test_labels = twenty_test.target
+
+all_data = twenty_all.data
+all_labels = twenty_all.target
+
+#%%
+result = []
+for data in all_data:
+    # 1. convert all letters to lower cases
+    temp = data.lower()
+    
+    # 2. remove numbers
+    temp = re.sub(r'\d+', '', temp)
+
+    # 3. remove punctuations, accent marks and other diacritics
+    temp = temp.translate(str.maketrans('','',string.punctuation))
+
+    # 4. remove leading and ending white spaces
+    temp = temp.strip()
+    
+    # 5. remove stop words
+    stops = set(stopwords.words("english"))
+    tokens = word_tokenize(temp)
+    filtered_words = [word for word in tokens if word not in stops]
+    
+    # 6. word stemming (only use root words)
+    stemmer= PorterStemmer()
+    result_word = []
+
+    for word in filtered_words:
+        root_word = stemmer.stem(word)
+        result_word.append(root_word)
+
+    result.append(result_word)
+    
+    
+#%%
+#result_test = []
+#for data in test_data:
+#    # 1. convert all letters to lower cases
+#    temp = data.lower()
+#    
+#    # 2. remove numbers
+#    temp = re.sub(r'\d+', '', temp)
+#
+#    # 3. remove punctuations, accent marks and other diacritics
+#    temp = temp.translate(str.maketrans('','',string.punctuation))
+#
+#    # 4. remove leading and ending white spaces
+#    temp = temp.strip()
+#    
+#    # 5. remove stop words
+#    stops = set(stopwords.words("english"))
+#    tokens = word_tokenize(temp)
+#    filtered_words = [word for word in tokens if word not in stops]
+#    
+#    # 6. word stemming (only use root words)
+#    stemmer= PorterStemmer()
+#    result_word = []
+#
+#    for word in filtered_words:
+#        root_word = stemmer.stem(word)
+#        result_word.append(root_word)
+#
+#    result_test.append(result_word)
+    
+#%%    
+vectorizer = TfidfVectorizer(analyzer='word', tokenizer = dummy_tok, 
+                             preprocessor = dummy_tok,
+                             token_pattern = None)
+
+X_all = vectorizer.fit_transform(result).toarray()
+#print(X_all.shape) #1963
+X_train = X_all[0:len(train_data)]
+X_test = X_all[len(train_data):len(X_all)]
+
+# Initialize the Classifier and start training
+RFC = RandomForestClassifier(n_estimators=300, random_state=0)
+#cv_results = cross_validate(RFC, X, y, cv=5)
+
+RFC.fit(X_train, train_labels)
+predicted = RFC.predict(X_test)
+
+accuracy = RFC.score(X_test, test_labels)
+print(accuracy)  #0.48025477
+
+#print(X.shape) #(1178, 18399)
+#print(len(result)) #(1178)
+#print(cv_results['test_score']) #(0.9449, 0.9534, 0.9194, 0.9194, 0.9529)
+
+
+
--- Project2/Preprocess.py
+++ Project2/Preprocess.py
@@ -14,99 +14,71 @@ nltk.download('stopwords')
 nltk.download('punkt')
 
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_validate
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 
 
-#root_path = '/Users/j.li/School/U4_WINTER/COMP 551/Applied_Machine_Learning/Project2/'
-
-#%% 
-# Define a dummy tokenizer for TF-IDF Vectorizer
-def dummy_tok(input):
-    return input
-
 #%%
 from sklearn.datasets import fetch_20newsgroups
-twenty_train_all = fetch_20newsgroups(subset='train', shuffle=True, random_state=42)
-all_names = twenty_train_all.target_names
-#all_labels = np.unique(twenty_train_all.target)
-
-train_data = twenty_train_all.data
-labels = twenty_train_all.target
-
-#print(twenty_train_all.data[0])
+#twenty_train_all = fetch_20newsgroups(subset='train', shuffle=True, random_state=42)
+#all_names = twenty_train_all.target_names
 #
-print(all_names)
-#print(all_labels)
+#train_data = twenty_train_all.data
+#labels = twenty_train_all.target
+#
+#print(all_names)
 
 
-# only test with a few groups
+#-----------------------only test with a few groups------------------------
+categories = ['alt.atheism','comp.graphics', 'misc.forsale','rec.autos', 'sci.med',
+              'soc.religion.christian','talk.politics.guns','talk.politics.mideast']
 #categories = ['comp.graphics', 'sci.med']
-#twenty_train = fetch_20newsgroups(subset='train', categories=categories, 
-#                                  shuffle=True, random_state=42)
-#train_data = twenty_train.data
-#labels = twenty_train.target
+twenty_train = fetch_20newsgroups(subset='train', categories=categories, 
+                                  shuffle=True, random_state=42)
+twenty_test = fetch_20newsgroups(subset='test', categories=categories, 
+                                 shuffle=True, random_state=42)
+twenty_all = fetch_20newsgroups(subset='all', categories=categories, 
+                                 shuffle=True, random_state=42)
+train_data = twenty_train.data
+train_labels = twenty_train.target
 
+test_data = twenty_test.data
+test_labels = twenty_test.target
+
+all_data = twenty_all.data
+all_labels = twenty_all.target
 
 
 
 #%%
-# Text-Preprocessing Tasks
-# 1. converting all letters to lower or upper case
-# 2. converting numbers into words or removing numbers
-# 3. removing punctuations, accent marks and other diacritics
-# 4. removing white spaces
-# 5. expanding abbreviations
-# 6. removing stop words, sparse terms, and particular words
-# 7. text canonicalization (use only root words)
+count_vect = CountVectorizer(analyzer='word', stop_words='english')
 
-result = []
-for data in train_data:
-    # 1. convert all letters to lower cases
-    temp = data.lower()
-    
-    # 2. remove numbers
-    temp = re.sub(r'\d+', '', temp)
+tfidf_transformer = TfidfTransformer()
+X_train_counts = count_vect.fit_transform(twenty_train.data)
+X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+print(X_train_counts.shape)
+print(X_train_tfidf.shape)
+print(len(train_data))
+#print(count_vect.get_feature_names())
 
-    # 3. remove punctuations, accent marks and other diacritics
-    temp = temp.translate(str.maketrans('','',string.punctuation))
 
-    # 4. remove leading and ending white spaces
-    temp = temp.strip()
-    
-    # 5. remove stop words
-    stops = set(stopwords.words("english"))
-    tokens = word_tokenize(temp)
-    filtered_words = [word for word in tokens if word not in stops]
-    
-    # 6. text canonicalization (only use root words)
-    stemmer= PorterStemmer()
-    result_word = []
-
-    for word in filtered_words:
-        root_word = stemmer.stem(word)
-        result_word.append(root_word)
-
-    result.append(result_word)
-    
-    
-vectorizer = TfidfVectorizer(analyzer='word', tokenizer = dummy_tok, 
-                             preprocessor = dummy_tok,
-                             token_pattern = None) 
-X = vectorizer.fit_transform(result).toarray()
-features = vectorizer.get_feature_names()
-
-y = labels
 
 # Initialize the Classifier and start training
 RFC = RandomForestClassifier(n_estimators=300, random_state=0)
-cv_results = cross_validate(RFC, X, y, cv=5)
-print(cv_results['test_score'])
+#cv_results = cross_validate(RFC, X, y, cv=5)
+#print(cv_results['test_score'])
 
+#%%
+from sklearn.pipeline import Pipeline
+text_clf = Pipeline([('vect', count_vect),
+                     ('tfidf', tfidf_transformer),
+                     ('clf', RFC)])
+text_clf.fit(train_data, train_labels)
 
-
-
-
+predicted = text_clf.predict(test_data)
+print(np.mean(predicted == test_labels))
 
