48d738dcd891a2c0a8e22419cdbb3a8e4c3ad7d4
Jessica Li <yihui.li@mail.mcgill.ca>
2020-03-05 11:06:55 -0500

    updated 20news code

--- Project2/Preprocess_20news.py
+++ Project2/Preprocess_20news.py
@@ -28,25 +28,25 @@ from sklearn.metrics import classification_report, confusion_matrix, accuracy_sc
 from sklearn.datasets import fetch_20newsgroups
 
 #-----------------------test all 20 groups--------------------------------
-#twenty_train = fetch_20newsgroups(subset='train', 
-#                                  shuffle=True, random_state=42)
-#twenty_test = fetch_20newsgroups(subset='test',
-#                                 shuffle=True, random_state=42)
-#twenty_all = fetch_20newsgroups(subset='all',
-#                                 shuffle=True, random_state=42)
+twenty_train = fetch_20newsgroups(subset='train', 
+                                  shuffle=True, random_state=42)
+twenty_test = fetch_20newsgroups(subset='test',
+                                 shuffle=True, random_state=42)
+twenty_all = fetch_20newsgroups(subset='all',
+                                 shuffle=True, random_state=42)
 
 #-----------------------only test with a few groups------------------------
 #categories = ['alt.atheism','comp.graphics', 'misc.forsale','rec.autos', 'sci.med',
 #              'soc.religion.christian','talk.politics.guns','talk.politics.mideast']
-categories = ['alt.atheism','comp.graphics', 'rec.autos', 'sci.med']
+#categories = ['alt.atheism','comp.graphics', 'rec.autos', 'sci.med']
 #categories = ['comp.graphics', 'sci.med']
 
-twenty_train = fetch_20newsgroups(subset='train', categories=categories, 
-                                  shuffle=True, random_state=42)
-twenty_test = fetch_20newsgroups(subset='test', categories=categories, 
-                                 shuffle=True, random_state=42)
-twenty_all = fetch_20newsgroups(subset='all', categories=categories, 
-                                 shuffle=True, random_state=42)
+#twenty_train = fetch_20newsgroups(subset='train', categories=categories, 
+#                                  shuffle=True, random_state=42)
+#twenty_test = fetch_20newsgroups(subset='test', categories=categories, 
+#                                 shuffle=True, random_state=42)
+#twenty_all = fetch_20newsgroups(subset='all', categories=categories, 
+#                                 shuffle=True, random_state=42)
 
 train_data = twenty_train.data
 train_labels = twenty_train.target
@@ -94,7 +94,8 @@ X_train = X_all[0:len(train_data)]
 X_test = X_all[len(train_data):len(X_all)]
 
 # Initialize the Classifier and start training
-RFC = RandomForestClassifier()
+RFC = RandomForestClassifier(n_estimators=500, min_samples_split=2, min_samples_leaf = 1,
+                             max_features='auto', max_depth = None, bootstrap=False, random_state=0)
 adaBoost = AdaBoostClassifier(n_estimators=300, random_state=0)
 #cv_results = cross_validate(RFC, X, y, cv=5)
 #print(cv_results['test_score'])
@@ -102,51 +103,51 @@ adaBoost = AdaBoostClassifier(n_estimators=300, random_state=0)
 #%%
 
 # Perform grid search to find best parameters
-from sklearn.model_selection import RandomizedSearchCV
-
-# Number of trees in random forest
-n_estimators = [int(x) for x in np.linspace(start = 50, stop = 400, num = 8)]
-
-# Number of features to consider at every split
-max_features = ['auto', 'sqrt']
-
-# Maximum number of levels in tree
-max_depth = [int(x) for x in np.linspace(10, 110, num = 11)]
-max_depth.append(None)
-
-# Minimum number of samples required to split a node
-min_samples_split = [2, 5, 10]
-
-# Minimum number of samples required at each leaf node
-min_samples_leaf = [1, 2, 4]
-
-# Method of selecting samples for training each tree
-bootstrap = [True, False]
-
-# Create the random grid
-random_grid = {'n_estimators': n_estimators,
-               'max_features': max_features,
-               'max_depth': max_depth,
-               'min_samples_split': min_samples_split,
-               'min_samples_leaf': min_samples_leaf,
-               'bootstrap': bootstrap}
-
-rf_random = RandomizedSearchCV(estimator = RFC, param_distributions = random_grid, n_iter = 50, cv = 3, verbose=2, random_state=42, n_jobs = -1)
-# Fit the random search model
-rf_random.fit(X_train, train_labels)
-
-print(rf_random.best_params_)
+#from sklearn.model_selection import RandomizedSearchCV
+#
+## Number of trees in random forest
+#n_estimators = [int(x) for x in np.linspace(start = 50, stop = 400, num = 8)]
+#
+## Number of features to consider at every split
+#max_features = ['auto', 'sqrt']
+#
+## Maximum number of levels in tree
+#max_depth = [int(x) for x in np.linspace(10, 110, num = 11)]
+#max_depth.append(None)
+#
+## Minimum number of samples required to split a node
+#min_samples_split = [2, 5, 10]
+#
+## Minimum number of samples required at each leaf node
+#min_samples_leaf = [1, 2, 4]
+#
+## Method of selecting samples for training each tree
+#bootstrap = [True, False]
+#
+## Create the random grid
+#random_grid = {'n_estimators': n_estimators,
+#               'max_features': max_features,
+#               'max_depth': max_depth,
+#               'min_samples_split': min_samples_split,
+#               'min_samples_leaf': min_samples_leaf,
+#               'bootstrap': bootstrap}
+#
+#rf_random = RandomizedSearchCV(estimator = RFC, param_distributions = random_grid, n_iter = 50, cv = 3, verbose=2, random_state=42, n_jobs = -1)
+## Fit the random search model
+#rf_random.fit(X_train, train_labels)
+#
+#print(rf_random.best_params_)
 
 
 #%%
-#from sklearn.pipeline import Pipeline
-#text_clf = Pipeline([('vect', tfidf_vect),
-##                     ('tfidf', tfidf_transformer),
-#                     ('clf', RFC)])
-#text_clf.fit(train_data, train_labels)
-#
-#predicted = text_clf.predict(test_data)
+from sklearn.pipeline import Pipeline
+text_clf = Pipeline([('vect', tfidf_vect),
+#                     ('tfidf', tfidf_transformer),
+                     ('clf', RFC)])
+text_clf.fit(train_data, train_labels)
+
+predicted = text_clf.predict(test_data)
 #accuracy = text_clf.score(test_data, test_labels)
 #print(accuracy)
-#print(np.mean(predicted == test_labels))
+print(np.mean(predicted == test_labels))
 
