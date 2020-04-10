8bdb1bb6b0b9f859b7edbaa7b5722d04752dee0f
Jessica Li <yihui.li@mail.mcgill.ca>
2020-04-10 15:09:03 -0400

    added preliminary MLP code

---
+++ Project3/main.py
@@ -0,0 +1,16 @@
+import pickle
+import os
+
+def unpickle(file):
+    with open(file, 'rb') as fo:
+        dict = pickle.load(fo, encoding='bytes')
+    return dict
+
+root_path = '/Users/j.li/School/U4_WINTER/COMP 551/Applied_Machine_Learning/Project3/cifar-10-batches-py/data_batch/'
+data_dir = os.listdir(root_path)
+dict_ls = []
+
+# All train and test data are loaded as a list of dict
+for i in range(len(data_dir)):
+    final_path = os.path.join(root_path, data_dir[i])
+    dict_ls.append(unpickle(final_path))
