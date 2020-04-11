53afa23cbdaf0a5abf2b7567cd03ec4633e0b1d7
clairel0130 <Lxj302552560>
2020-04-11 18:13:33 -0400

    Claire declaration

--- Project3/main.py
+++ Project3/main.py
@@ -5,9 +5,9 @@ def unpickle(file):
     with open(file, 'rb') as fo:
         dict = pickle.load(fo, encoding='bytes')
     return dict
-
-root_path = '/Users/j.li/School/U4_WINTER/COMP 551/Applied_Machine_Learning/Project3/cifar-10-batches-py/data_batch/'
-data_dir = os.listdir(root_path)
+root_path = '/Users/liuxijun/Downloads/Applied_Machine_Learning/Project3/cifar-10-batches-py/data_batch/'
+#root_path = '/Users/j.li/School/U4_WINTER/COMP 551/Applied_Machine_Learning/Project3/cifar-10-batches-py/data_batch/'
+data_dir = sorted(os.listdir(root_path))
 dict_ls = []
 
 # All train and test data are loaded as a list of dict
