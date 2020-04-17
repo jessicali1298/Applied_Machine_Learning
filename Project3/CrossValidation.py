fixed accuracy calculation

Project3/CrossValidation.py
@@ -33,33 +33,35 @@ class CrossValidation:
         fold_size = int(np.floor(X.shape[0]/k))
         accuracy_ls = []
         
-        if (act_func == 'ReLu'): 
-            folds_X, folds_Y = self.create_mini_batch(X, Y, fold_size)
-            folds_X = np.asarray(folds_X)
-            folds_Y = np.asarray(folds_Y)
-            
-            for i in range(k):
-                X_test = folds_X[i]
-                Y_test = folds_Y[i]
-                X_train = np.concatenate(np.delete(folds_X,i,0), axis=0)
-                Y_train = np.concatenate(np.delete(folds_Y,i,0), axis=0)
-                
-                N,D = X_train.shape
-                N,K = Y_train.shape
-                W = np.random.randn(M, K) * 0.01
-                V = np.random.randn(D, M) * 0.01
-                mlp_nn = mlp.MLP(W,V)
-                
-                mlp_nn.fit(X_train, Y_train, M, lr, max_iters, batch_size)
-                accuracy = mlp_nn.predict(X_test, Y_test, act_func)
-                accuracy_ls.append(accuracy)
+         
+        folds_X, folds_Y = self.create_mini_batch(X, Y, fold_size)
+        folds_X = np.asarray(folds_X)
+        folds_Y = np.asarray(folds_Y)
         
-        elif (act_func == 'Tanh'):
-            # do something
-            print('Using Tanh')
+        for i in range(k):
+            X_test = folds_X[i]
+            Y_test = folds_Y[i]
+            X_train = np.concatenate(np.delete(folds_X,i,0), axis=0)
+            Y_train = np.concatenate(np.delete(folds_Y,i,0), axis=0)
             
-        elif(act_func == 'Sigmoid'):
-            # do something
-            print('Using Sigmoid')
+            N,D = X_train.shape
+            N,K = Y_train.shape
+            W = np.random.randn(M, K) * 0.01
+            V = np.random.randn(D, M) * 0.01
+            mlp_nn = mlp.mlp(W,V)
+            
+            mlp_nn.fit(X_train, Y_train, M, lr, max_iters, batch_size, act_func)
+            predictions, accuracy = mlp_nn.predict(X_test, Y_test, act_func)
+            accuracy_ls.append(accuracy)
+            
+#        if (act_func == 'ReLu'):
+#            
+#        elif (act_func == 'Tanh'):
+#            # do something
+#            print('Using Tanh')
+#            
+#        elif(act_func == 'Sigmoid'):
+#            # do something
+#            print('Using Sigmoid')
     
         return accuracy_ls, np.mean(accuracy_ls)

Project3/main.py
@@ -47,32 +47,38 @@ for i in range(1,4):
 
 Y_train = one_hot(Y_train)
 
-M = 10          # number of hiddne units
+M = 200          # number of hidden units
 lr = 0.1/10000  # learning rate
 eps = 1e-9
-max_iters = 15
-batch_size = 500
+max_iters = 40
+batch_size = 40
 
 N,D = X_train.shape
 N,K = Y_train.shape
 W = np.random.randn(M, K) * 0.01
 V = np.random.randn(D, M) * 0.01
 
-mlp_nn = mlp.MLP(W,V)
+mlp_nn = mlp.mlp(W,V)
 #%%
-#mlp_nn.fit(X_train, Y_train, M, lr, max_iters, batch_size)
-#
-#
-#Wh = mlp_nn.W
-#Vh = mlp_nn.V
-#
-## test the model
-#accuracy = mlp_nn.predict(X_test, Y_test, 'ReLu')
+mlp_nn.fit(X_train, Y_train, M, lr, max_iters, batch_size, 'ReLu')
+
+
+Wh = mlp_nn.W
+Vh = mlp_nn.V
+
+# test the model
+predictions, accuracy = mlp_nn.predict(X_test, Y_test, 'ReLu')
 
 #%%
 # 5-fold cross validation
-cv_obj = cv.CrossValidation()
-cv_accuracy, avg_cv_accuracy = cv_obj.cross_validation(X_train, Y_train, M, lr, max_iters, batch_size, 5, 'ReLu')
+#cv_obj = cv.CrossValidation()
+#cv_accuracy, avg_cv_accuracy = cv_obj.cross_validation(X_train, Y_train, M, lr, max_iters, batch_size, 5, 'ReLu')
+
+
+#temp1 = np.argmax(np.array([[0,0,0,1,0,0], [1,0,0,0,0,0], [0,1,0,0,0,0]]),axis=1)
+#temp2 = np.argmax(np.array([[0,0,0,1,0,0], [1,0,0,0,0,0], [1,0,0,0,0,0]]), axis=1)
+#temp3 = np.array([[0,0,0,1,0,0], [1,0,0,0,0,0], [0,1,0,0,0,0]])
+#result = np.mean(temp1 == temp2)
 
 #folds_X, folds_Y = cv_obj.create_mini_batch(X_train, Y_train, 8000)
 #X_test = folds_X[0]

Project3/mlp.py
@@ -102,6 +102,37 @@ class mlp:
                   X, #N x D
                   Y, #N x K
                   W, #M x K 
+                  V,  #D x M
+                  act_func
+                  ):
+#        print('X: ', X.shape)
+#        print('Y: ', Y.shape)
+#        print('W: ', W.shape)
+#        print('V: ', V.shape)
+        
+        if act_func == 'ReLu':
+            Z = self.ReLu(np.dot(X,V)) #N x M     10000 x 10, hidden layer
+            
+            N,D = X.shape
+            Yh = self.softmax(np.dot(Z,W)) #N x K     10000 x 10
+            
+            dY = Yh - Y     #N x K     10000 x 10
+            
+            dW = np.dot(Z.T, dY)/N  #M x K     10 x 10 
+            
+            dZ = np.dot(dY, W.T)    #N x M     10000 x 10
+            
+            hidden_grad = self.ReLuGrad(Z)
+           
+        dV = np.dot(X.T, dZ * hidden_grad)/N
+
+        return dW, dV
+    
+        # assume middle layer activation function is ReLu
+    def gradients_old(self,
+                  X, #N x D
+                  Y, #N x K
+                  W, #M x K 
                   V  #D x M
                   ):
 #        print('X: ', X.shape)
@@ -133,8 +164,7 @@ class mlp:
 #        print('')
         return dW, dV
     
-    
-    def mini_GD(self, X, Y, M, lr, max_iters, batch_size):
+    def mini_GD(self, X, Y, M, lr, max_iters, batch_size, act_func):
 
         N,D = X.shape
         N,K = Y.shape
@@ -151,34 +181,17 @@ class mlp:
                 mini_X = batch[0]
                 mini_Y = batch[1]
                 
-                dW, dV = self.gradients(mini_X, mini_Y, W, V)
+                dW, dV = self.gradients(mini_X, mini_Y, W, V, act_func)
 #                print('dW: ', dW.shape)
 #                print('W: ', W.shape)
                 W = W - lr*dW
                 V = V - lr*dV
                 t = t + 1
         return W, V
-   
-    
-    def GD(self, X, Y, M, lr, eps, max_iters):
-        Y = self.one_hot(Y)
-        N,D = X.shape
-        N,K = Y.shape
-        W = np.random.randn(M, K) * 0.01
-        V = np.random.randn(D, M) * 0.01
-        dW = np.inf * np.ones_like(W)
-        t = 0
-
-        while np.linalg.norm(dW) > eps and t < max_iters:
-            dW, dV = self.gradients(X, Y, W, V)
-            W = W - lr*dW
-            V = V - lr*dV
-            t += 1
-        return W, V
     
     
-    def fit(self, X, Y, M, lr, max_iters, batch_size):
-        W, V = self.mini_GD(X, Y, M, lr, max_iters, batch_size)
+    def fit(self, X, Y, M, lr, max_iters, batch_size, act_func):
+        W, V = self.mini_GD(X, Y, M, lr, max_iters, batch_size, act_func)
         self.W = W
         self.V = V
     
@@ -186,7 +199,8 @@ class mlp:
     def predict(self, X, Y, act_func):
         if (act_func == 'ReLu'): 
             softmax_result = self.softmax(np.dot(self.ReLu(np.dot(X, self.V)), self.W))
-            result = self.one_hot(np.argmax(softmax_result, axis=1))
+            
+            
         elif (act_func == 'Tanh'):
             # do something
             print('Using Tanh')
@@ -194,7 +208,8 @@ class mlp:
         elif(act_func == 'Sigmoid'):
             # do something
             print('Using Sigmoid')
-            
-        accuracy = np.mean(result == Y)
-        return accuracy
+        result = np.argmax(self.one_hot(np.argmax(softmax_result, axis=1)), axis=1)  
+        Y_decode = np.argmax(Y, axis=1) 
+        accuracy = np.mean(result == Y_decode)
+        return result, accuracy
     